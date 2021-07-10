056897143906fa938db496a672f42c7d34f09046
WastPow <48478380+WastPow@users.noreply.github.com>
2021-07-10 14:57:48 +0200

    Update Uppgift 2.py

--- Uppgift 2.py
+++ Uppgift 2.py
@@ -1,7 +1,7 @@
 #Uppgift 2
 """
-Summan av de tio första naturliga talens kvadrater är 12+22+...+102= 385
-Summan av de tio första naturliga talen i kvadrat är (1+2+...+10)2=552=3025
+Summan av de tio första naturliga talens kvadrater är 1^2+2^2+...+10^2=385
+Summan av de tio första naturliga talen i kvadrat är (1+2+...+10)^2=552=3025
 Därmed är differensen av summorna 3025-385=2640.
 Beräkna differensen mellan summan av alla naturliga tals kvadrater från 1 → 100 och kvadraten av summan från alla tal mellan 1 → 100.
 
